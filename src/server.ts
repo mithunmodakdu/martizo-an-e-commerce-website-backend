0939cc0bf7b48d877577ca184e2fce3e082336ef
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2025-10-26 15:40:06 +0600

    Error Handlers for Server: UnhandledRejection, UncaughtException, SIGTERM, SIGINT

--- src/server.ts
+++ src/server.ts
@@ -24,3 +24,51 @@ const startServer = async() => {
 
 startServer();
 
+process.on("unhandledRejection", (error) => {
+  console.log("Unhandled rejection detected. Server is shutting down...", error);
+
+  if(server){
+    server.close(() => {
+      process.exit(1);
+    });   
+  }
+
+  process.exit(1);
+})
+
+process.on("uncaughtException", (error) => {
+  console.log("Uncaught exception occurred. Server is shutting down...", error );
+
+  if(server){
+    server.close(() => {
+      process.exit(1);
+    })
+  }
+
+  process.exit(1);
+})
+
+process.on("SIGTERM", () => {
+  console.log("SIGTERM signal received. Server is shutting down...")
+
+  if(server){
+    server.close(() => {
+      process.exit(1);
+    })
+  }
+
+  process.exit(1);
+})
+
+process.on("SIGINT", () => {
+  console.log("SIGINT signal received. Server is shutting down... ");
+
+  if(server){
+    server.close(() => {
+      process.exit(1);
+    })
+  }
+
+  process.exit(1);
+})
+
