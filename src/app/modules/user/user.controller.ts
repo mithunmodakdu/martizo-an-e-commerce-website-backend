baadc3702ced6ed0414e76101fa02f97fd1cca6e
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2025-10-27 16:59:31 +0600

    Custom AppError Class created

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -2,10 +2,13 @@
 import { NextFunction, Request, Response } from "express";
 import httpStatusCodes from "http-status-codes";
 import { UserServices } from "./user.service";
+// import AppError from "../../errorHelpers/AppError";
 
 const createUser = async(req: Request, res: Response, next: NextFunction) => {
   try {
-    throw new Error("fake error")
+    // throw new Error("fake error")
+    // throw new AppError(httpStatusCodes.BAD_REQUEST, "this is fake app error")
+    
     const user = await UserServices.createUser(req.body);
 
     res.status(httpStatusCodes.CREATED).json({
