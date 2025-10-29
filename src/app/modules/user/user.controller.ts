ba94c098b5f30461951cddb5268d559ccb9ce81b
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2025-10-29 14:33:13 +0600

    sendResponse utility created

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -3,15 +3,18 @@ import { NextFunction, Request, Response } from "express";
 import httpStatusCodes from "http-status-codes";
 import { UserServices } from "./user.service";
 import { catchAsync } from "../../utils/catchAsync";
+import { sendResponse } from "../../utils/sendResponse";
 
 
 const createUser = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     const user = await UserServices.createUser(req.body);
 
-    res.status(httpStatusCodes.CREATED).json({
+    sendResponse(res, {
+      statusCode: httpStatusCodes.CREATED,
+      success: true,
       message: "User created successfully.",
-      user,
+      data: user,
     });
   }
 );
