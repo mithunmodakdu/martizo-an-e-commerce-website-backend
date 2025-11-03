2f16de1d4fb63fd63ae73e4d9027f39d6eff627e
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2025-11-03 21:32:14 +0600

    Configuring Passport JS for Google Authentication

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -13,30 +13,35 @@ interface IEnvVars {
   JWT_REFRESH_EXPIRE_TIME: string;
   SUPER_ADMIN_EMAIL: string;
   SUPER_ADMIN_PASSWORD: string;
-  
+  GOOGLE_CLIENT_ID: string;
+  GOOGLE_CLIENT_SECRET: string;
+  GOOGLE_CALLBACK_URL: string;
+  EXPRESS_SESSION_SECRET: string;
 }
 
-const loadEnvVars = () : IEnvVars => {
-
-  const requiredEnvVars : string[] = [
-    "PORT", 
-    "DB_URL", 
-    "NODE_ENV", 
+const loadEnvVars = (): IEnvVars => {
+  const requiredEnvVars: string[] = [
+    "PORT",
+    "DB_URL",
+    "NODE_ENV",
     "BCRYPT_SALT_ROUND",
     "JWT_SECRET",
     "JWT_EXPIRE_TIME",
     "JWT_REFRESH_SECRET",
     "JWT_REFRESH_EXPIRE_TIME",
     "SUPER_ADMIN_EMAIL",
-    "SUPER_ADMIN_PASSWORD"
-  
+    "SUPER_ADMIN_PASSWORD",
+    "GOOGLE_CLIENT_ID",
+    "GOOGLE_CLIENT_SECRET",
+    "GOOGLE_CALLBACK_URL",
+    "EXPRESS_SESSION_SECRET",
   ];
 
   requiredEnvVars.forEach((key) => {
-    if(!process.env[key]){
-      throw new Error(`Required environment variable ${key} is missing.`)
+    if (!process.env[key]) {
+      throw new Error(`Required environment variable ${key} is missing.`);
     }
-  })
+  });
 
   return {
     PORT: process.env.PORT as string,
@@ -49,6 +54,10 @@ const loadEnvVars = () : IEnvVars => {
     JWT_REFRESH_EXPIRE_TIME: process.env.JWT_REFRESH_EXPIRE_TIME as string,
     SUPER_ADMIN_EMAIL: process.env.SUPER_ADMIN_EMAIL as string,
     SUPER_ADMIN_PASSWORD: process.env.SUPER_ADMIN_PASSWORD as string,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID as string,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET as string,
+    GOOGLE_CALLBACK_URL: process.env.GOOGLE_CALLBACK_URL as string,
+    EXPRESS_SESSION_SECRET: process.env.EXPRESS_SESSION_SECRET as string,
   };
 };
 
