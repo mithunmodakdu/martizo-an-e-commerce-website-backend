a6244678370db82e71fea84ef35d7f5dcca83461
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2025-11-09 14:45:16 +0600

    Handling Mongoose Cast Error and Duplicate Error

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -7,8 +7,20 @@ import AppError from "../errorHelpers/AppError";
 export const globalErrorHandler = (error: any, req: Request, res: Response, next: NextFunction) => {
   let statusCode = 500;
   let message = "Something went wrong";
-  
-  if(error instanceof AppError){
+    
+  if(error.code === 11000){
+    // console.log("Duplicate error:::", error)
+    statusCode = 400;
+    console.log(error.message)
+    const duplicate = error.message.match(/"([^"]*)"/);
+    // console.log(duplicate)
+    // console.log(duplicate[1])
+    message = `${duplicate[1]} already exists.`
+  }else if(error.name === "CastError"){
+    statusCode = 400;
+    message = "Invalid MongoDB ObjectID. Please provide a valid ObjectID.";
+  }
+  else if(error instanceof AppError){
     statusCode = error.statusCode;
     message = error.message
   }else if(error instanceof Error){
