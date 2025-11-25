3627b2007615ce8c251f0d66d7fb628075e96bea
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2025-11-25 11:57:28 +0600

    createBrandZodSchema completed

--- src/app/modules/brand/brand.route.ts
+++ src/app/modules/brand/brand.route.ts
@@ -1,8 +1,17 @@
 import { Router } from "express";
 import { BrandControllers } from "./brand.controller";
+import { checkAuth } from "../../middlewares/checkAuth";
+import { ERole } from "../user/user.interface";
+import { validateRequest } from "../../middlewares/validateRequest";
+import { createBrandZodSchema } from "./brand.validation";
 
 const router = Router();
 
-router.post("/create", BrandControllers.createBrand);
+router.post(
+  "/create",
+  checkAuth(ERole.SUPER_ADMIN),
+  validateRequest(createBrandZodSchema),
+  BrandControllers.createBrand
+);
 
 export const BrandRoutes = router;
