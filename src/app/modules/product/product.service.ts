f27203cf5c58b4232d084ca8c43f1caf8c1786fa
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2025-11-30 16:06:50 +0600

    meta data creation including totalPage, query in another style

--- src/app/modules/product/product.service.ts
+++ src/app/modules/product/product.service.ts
@@ -4,19 +4,22 @@ import { IProduct } from "./product.interface";
 import { Product } from "./product.model";
 import httpStatusCodes from "http-status-codes";
 
-const createProduct = async(payload: Partial<IProduct>) =>{
-  const existedProduct = await Product.findOne({title: payload.title});
+const createProduct = async (payload: Partial<IProduct>) => {
+  const existedProduct = await Product.findOne({ title: payload.title });
 
-  if(existedProduct){
-    throw new AppError(httpStatusCodes.BAD_REQUEST, "This product already exists")
+  if (existedProduct) {
+    throw new AppError(
+      httpStatusCodes.BAD_REQUEST,
+      "This product already exists"
+    );
   }
 
   const product = await Product.create(payload);
 
   return product;
-}
+};
 
-const getAllProducts = async(query : Record<string, string>) => {
+const getAllProducts = async (query: Record<string, string>) => {
   const filter = query;
   const searchTerm = query.searchTerm || "";
   const sort = query.sort || "-createdAt";
@@ -25,29 +28,45 @@ const getAllProducts = async(query : Record<string, string>) => {
   const limit = Number(query.limit) || 5;
   const skip = (page - 1) * limit;
 
-  for(const field of excludeFields){
+  for (const field of excludeFields) {
     // eslint-disable-next-line @typescript-eslint/no-dynamic-delete
-    delete filter[field]
+    delete filter[field];
   }
 
   const searchQuery = {
     $or: productSearchableFields.map((field) => ({
-      [field] : {$regex: searchTerm, $options: "i"}
-    }))
-    
-  }
+      [field]: { $regex: searchTerm, $options: "i" },
+    })),
+  };
 
-  const products = await Product.find(searchQuery).find(filter).sort(sort).select(fields).limit(limit).skip(skip);
+  // const products = await Product.find(filter)
+  //   .find(searchQuery)
+  //   .sort(sort)
+  //   .select(fields)
+  //   .limit(limit)
+  //   .skip(skip);
+
+  const productsByFilter = Product.find(filter);
+  const productsBySearch = productsByFilter.find(searchQuery);
+  const products = await productsBySearch.sort(sort).select(fields).skip(skip).limit(limit);
 
   const totalProducts = await Product.countDocuments();
+  const totalPage = Math.ceil(totalProducts / limit);
+
+  const meta = {
+    page: page,
+    limit: limit,
+    total: totalProducts,
+    totalPage: totalPage,
+  };
 
   return {
-    meta: {total: totalProducts},
-    data: products
+    meta: meta,
+    data: products,
   };
-}
+};
 
 export const ProductServices = {
   createProduct,
-  getAllProducts
-}
+  getAllProducts,
+};
