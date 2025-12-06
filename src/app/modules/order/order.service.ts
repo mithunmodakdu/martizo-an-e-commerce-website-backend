9b8f2140398c67d636a63baa6b04d96b1594615c
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2025-12-06 15:30:58 +0600

    session transaction for Rollback implemented in order and payment creation

--- src/app/modules/order/order.service.ts
+++ src/app/modules/order/order.service.ts
@@ -13,72 +13,90 @@ const createTransactionId = () => {
 // console.log(createTransactionId())
 
 const createOrder = async (userId: string, payload: Partial<IOrder>) => {
-  const { shippingAddress, paymentMethod } = payload;
+  const session = await Order.startSession();
+  session.startTransaction();
 
-  if (!shippingAddress) {
-    const user = await User.findOne({ _id: userId });
+  try {
+    const { shippingAddress, paymentMethod } = payload;
 
-    if (!user?.phone || !user.address) {
-      throw new AppError(
-        httpStatusCodes.BAD_REQUEST,
-        "Please update your profile with phone number and address."
-      );
+    if (!shippingAddress) {
+      const user = await User.findOne({ _id: userId });
+
+      if (!user?.phone || !user.address) {
+        throw new AppError(
+          httpStatusCodes.BAD_REQUEST,
+          "Please update your profile with phone number and address."
+        );
+      }
     }
+
+    const cart = await Cart.findOne({ userId });
+
+    if (!cart || cart.items.length === 0) {
+      throw new AppError(httpStatusCodes.BAD_REQUEST, "Your cart is empty.");
+    }
+
+    const orderItems = cart.items.map((item) => ({
+      productId: item.productId,
+      name: item.name,
+      quantity: item.quantity,
+      price: item.price,
+      variant: item.variant,
+    }));
+
+    const taxPrice = cart.itemsPrice * 0.15;
+    const shippingPrice =
+      payload.shippingAddress?.city?.toLowerCase() === "dhaka" ? 60 : 120;
+    const totalPrice = cart.itemsPrice + taxPrice + shippingPrice;
+
+    const orderData = {
+      userId,
+      shippingAddress,
+      paymentMethod,
+      items: orderItems,
+      itemsPrice: cart.itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    };
+
+    const order = await Order.create([orderData], { session });
+
+    const payment = await Payment.create(
+      [
+        {
+          userId,
+          orderId: order[0]._id,
+          amount: order[0].totalPrice,
+          transactionId: createTransactionId(),
+          paymentMethod,
+          status: EPaymentStatus.UNPAID,
+        },
+      ],
+      { session }
+    );
+
+    const updatedOrder = await Order.findByIdAndUpdate(
+      order[0]._id,
+      { paymentId: payment[0]._id },
+      { new: true, runValidators: true, session }
+    )
+      .populate("userId", "name email")
+      .populate("paymentId");
+
+
+    cart.items = [];
+    await cart.save({session});
+
+    await session.commitTransaction();
+    session.endSession();
+
+    return updatedOrder;
+  } catch (error) {
+    await session.abortTransaction();
+    session.endSession();
+    throw error;
   }
-
-  const cart = await Cart.findOne({ userId });
-
-  if (!cart || cart.items.length === 0) {
-    throw new AppError(httpStatusCodes.BAD_REQUEST, "Your cart is empty.");
-  }
-
-  const orderItems = cart.items.map((item) => ({
-    productId: item.productId,
-    name: item.name,
-    quantity: item.quantity,
-    price: item.price,
-    variant: item.variant,
-  }));
-
-  const taxPrice = cart.itemsPrice * 0.15;
-  const shippingPrice =
-    payload.shippingAddress?.city?.toLowerCase() === "dhaka" ? 60 : 120;
-  const totalPrice = cart.itemsPrice + taxPrice + shippingPrice;
-
-  const orderData = {
-    userId,
-    shippingAddress,
-    paymentMethod,
-    items: orderItems,
-    itemsPrice: cart.itemsPrice,
-    taxPrice,
-    shippingPrice,
-    totalPrice,
-  };
-
-  const order = await Order.create(orderData);
-
-  const payment = await Payment.create({
-    userId,
-    orderId: order._id,
-    amount: order.totalPrice,
-    transactionId: createTransactionId(),
-    paymentMethod,
-    status: EPaymentStatus.UNPAID,
-  });
-
-  const updatedOrder = await Order.findByIdAndUpdate(
-    order._id,
-    { paymentId: payment._id },
-    { new: true, runValidators: true }
-  )
-    .populate("userId", "name email")
-    .populate("paymentId");
-
-  cart.items = [];
-  await cart.save();
-
-  return updatedOrder;
 };
 
 export const OrderServices = {
