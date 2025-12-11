22e11b5e7c6eb2bd015381a2d40b926abb711fc0
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2025-12-11 16:40:21 +0600

    Delete Image from Cloudinary if API throws error

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,46 +1,60 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { NextFunction, Request, Response } from "express"
-import { envVars } from "../config/env"
+import { NextFunction, Request, Response } from "express";
+import { envVars } from "../config/env";
 import AppError from "../errorHelpers/AppError";
 import { handleZodError } from "../errorHelpers/handleZodError";
 import { IErrorSource } from "../errorHelpers/error.interfaces";
 import { handleDuplicateError } from "../errorHelpers/handleDuplicateError";
 import { handleCastError } from "../errorHelpers/handleCastError";
 import { handleValidationError } from "../errorHelpers/handleValidationError";
+import { deleteImageFromCloudinary } from "../config/cloudinary.config";
 
-export const globalErrorHandler = (error: any, req: Request, res: Response, next: NextFunction) => {
+export const globalErrorHandler = async (
+  error: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (req.file) {
+    await deleteImageFromCloudinary(req.file.path);
+  }
+
+  if (req.files && Array.isArray(req.files) && req.files.length > 0) {
+    const imageUrls = (req.files as Express.Multer.File[]).map(
+      (file) => file.path
+    );
+
+    await Promise.all(imageUrls.map((url) => deleteImageFromCloudinary(url)));
+  }
+
   let statusCode = 500;
   let message = "Something went wrong";
-  let errorSources : IErrorSource[] = [];
+  let errorSources: IErrorSource[] = [];
 
-  if(error.name === "ZodError"){
+  if (error.name === "ZodError") {
     const simplifiedError = handleZodError(error);
     statusCode = simplifiedError.statusCode;
     message = simplifiedError.message;
     errorSources = simplifiedError.errorSources as IErrorSource[];
     error = error.issues;
-  }
-  else if(error.code === 11000){
+  } else if (error.code === 11000) {
     const simplifiedError = handleDuplicateError(error);
     statusCode = simplifiedError.statusCode;
     message = simplifiedError.message;
-
-  }else if(error.name === "CastError"){
+  } else if (error.name === "CastError") {
     const simplifiedError = handleCastError(error);
     statusCode = simplifiedError.statusCode;
     message = simplifiedError.message;
-
-  }else if(error.name === "ValidationError"){
+  } else if (error.name === "ValidationError") {
     const simplifiedError = handleValidationError(error);
     statusCode = simplifiedError.statusCode;
     message = simplifiedError.message;
     errorSources = simplifiedError.errorSources as IErrorSource[];
-  }
-  else if(error instanceof AppError){
+  } else if (error instanceof AppError) {
     statusCode = error.statusCode;
-    message = error.message
-  }else if(error instanceof Error){
+    message = error.message;
+  } else if (error instanceof Error) {
     statusCode = 500;
     message = error.message;
   }
@@ -49,9 +63,7 @@ export const globalErrorHandler = (error: any, req: Request, res: Response, next
     success: false,
     message,
     errorSources,
-    error,   
-    stack: envVars.NODE_ENV === "development"? error.stack : null
-  })
-
-
-}
+    error,
+    stack: envVars.NODE_ENV === "development" ? error.stack : null,
+  });
+};
