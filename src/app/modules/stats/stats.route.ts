be5d791d7d107e633733c935ddf99678e0b24462
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2025-12-19 22:04:59 +0600

    getUsersStats api completed

--- src/app/modules/stats/stats.route.ts
+++ src/app/modules/stats/stats.route.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { StatsControllers } from "./stats.controller";
+import { checkAuth } from "../../middlewares/checkAuth";
+import { ERole } from "../user/user.interface";
 
 const router = Router();
 
-router.get("/users", StatsControllers.getUsersStats);
+router.get("/users", checkAuth(ERole.SUPER_ADMIN, ERole.ADMIN),  StatsControllers.getUsersStats);
 router.get("/products", StatsControllers.getProductsStats);
 router.get("/orders", StatsControllers.getOrdersStats);
 router.get("/payments", StatsControllers.getPaymentsStats);
