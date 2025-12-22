6eec38598d05c1aad5a5e170038b7d2d0867e533
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2025-12-22 23:58:12 +0600

    totalHighestOrderedProductsPromise completed

--- src/app/modules/stats/stats.service.ts
+++ src/app/modules/stats/stats.service.ts
@@ -1,26 +1,37 @@
+import { Order } from "../order/order.model";
 import { Product } from "../product/product.model";
 import { EIsActive } from "../user/user.interface";
 import { User } from "../user/user.model";
 
 const now = new Date();
-const sevenDaysAgo = new Date(now).setDate(now.getDate()-7);
-const thirtyDaysAgo = new Date(now).setDate(now.getDate()-30);
+const sevenDaysAgo = new Date(now).setDate(now.getDate() - 7);
+const thirtyDaysAgo = new Date(now).setDate(now.getDate() - 30);
 
-const getUsersStats = async() => {
-  const totalActiveUsersPromise = User.countDocuments({isActive: EIsActive.ACTIVE});
-  const totalInactiveUsersPromise = User.countDocuments({isActive: EIsActive.INACTIVE});
-  const totalBlockedUsersPromise = User.countDocuments({isActive: EIsActive.BLOCKED});
+const getUsersStats = async () => {
+  const totalActiveUsersPromise = User.countDocuments({
+    isActive: EIsActive.ACTIVE,
+  });
+  const totalInactiveUsersPromise = User.countDocuments({
+    isActive: EIsActive.INACTIVE,
+  });
+  const totalBlockedUsersPromise = User.countDocuments({
+    isActive: EIsActive.BLOCKED,
+  });
 
-  const newTotalUsersInLastSevenDaysPromise = User.countDocuments({createdAt: {$gte: sevenDaysAgo}});
-  const newTotalUsersInLastThirtyDaysPromise = User.countDocuments({createdAt: {$gte: thirtyDaysAgo}});
+  const newTotalUsersInLastSevenDaysPromise = User.countDocuments({
+    createdAt: { $gte: sevenDaysAgo },
+  });
+  const newTotalUsersInLastThirtyDaysPromise = User.countDocuments({
+    createdAt: { $gte: thirtyDaysAgo },
+  });
 
   const totalUsersByRolePromise = User.aggregate([
     {
-      $group : {
+      $group: {
         _id: "$role",
-        count: {$sum: 1}
-      }
-    }
+        count: { $sum: 1 },
+      },
+    },
   ]);
 
   const [
@@ -29,14 +40,14 @@ const getUsersStats = async() => {
     totalBlockedUsers,
     newTotalUsersInLastSevenDays,
     newTotalUsersInLastThirtyDays,
-    totalUsersByRole
+    totalUsersByRole,
   ] = await Promise.all([
     totalActiveUsersPromise,
     totalInactiveUsersPromise,
     totalBlockedUsersPromise,
     newTotalUsersInLastSevenDaysPromise,
     newTotalUsersInLastThirtyDaysPromise,
-    totalUsersByRolePromise
+    totalUsersByRolePromise,
   ]);
 
   return {
@@ -45,34 +56,33 @@ const getUsersStats = async() => {
     totalBlockedUsers,
     newTotalUsersInLastSevenDays,
     newTotalUsersInLastThirtyDays,
-    totalUsersByRole
-  }
-}
+    totalUsersByRole,
+  };
+};
 
-const getProductsStats = async() => {
-  const totalProductsPromise = Product.countDocuments(); 
+const getProductsStats = async () => {
+  const totalProductsPromise = Product.countDocuments();
 
   const totalProductsByCategoryPromise = Product.aggregate([
     {
-      $lookup : {
+      $lookup: {
         from: "categories",
         localField: "category",
         foreignField: "_id",
-        as: "category"
-      }
+        as: "category",
+      },
     },
 
     {
-      $unwind: "$category"
+      $unwind: "$category",
     },
 
     {
       $group: {
         _id: "$category.name",
-        count: {$sum: 1}
-      }
-    }
-  
+        count: { $sum: 1 },
+      },
+    },
   ]);
 
   const totalProductsByBrandNamePromise = Product.aggregate([
@@ -81,65 +91,124 @@ const getProductsStats = async() => {
         from: "brands",
         localField: "brand",
         foreignField: "_id",
-        as: "brand"
-      }
+        as: "brand",
+      },
     },
 
     {
-      $unwind: "$brand"
+      $unwind: "$brand",
     },
 
     {
       $group: {
         _id: "$brand.name",
-        count: {$sum: 1}
+        count: { $sum: 1 },
+      },
+    },
+  ]);
+
+  const totalHighestOrderedProductsPromise = Order.aggregate([
+    // stage-1: break items array
+    {
+      $unwind: "$items",
+    },
+
+    // stage-2: group by productId
+    {
+      $group: {
+        _id: "$items.productId",
+        totalOrders: { $sum: 1 },
+        totalQuantity: { $sum: "$items.quantity" },
+      },
+    },
+
+    // stage-3: sort by orders number
+    {
+      $sort: { totalOrders: -1 },
+    },
+
+    // stage-4: limit top ordered products
+    {
+      $limit: 5,
+    },
+
+    // stage-5: lookup product details
+    {
+      $lookup: {
+        from: "products",
+        let: { productId: "$_id" },
+        pipeline: [
+          {
+            $match: {
+              $expr: { $eq: ["$_id", "$$productId"] },
+            },
+          },
+        ],
+        as: "product",
+      },
+    },
+
+    // stage-6: unwind product array
+    {
+      $unwind: "$product"
+    },
+
+    // stage-7: final shape
+    {
+      $project: {
+        _id: 0,
+        productId: "$_id",
+        totalOrders: 1,
+        totalQuantity: 1,
+        "product.title": 1,
+        "product.slug": 1
       }
     }
   ]);
 
   const averageProductsPricePromise = Product.aggregate([
     {
-      $group : {
+      $group: {
         _id: null,
-        averageProductsPrice: {$avg: "$price"}
-      }
-    }
+        averageProductsPrice: { $avg: "$price" },
+      },
+    },
   ]);
 
-  const [totalProducts, 
+  const [
+    totalProducts,
     totalProductsByCategory,
     totalProductsByBrandName,
-    averageProductsPrice
-     
+    totalHighestOrderedProducts,
+    averageProductsPrice,
   ] = await Promise.all([
-    totalProductsPromise, 
+    totalProductsPromise,
     totalProductsByCategoryPromise,
     totalProductsByBrandNamePromise,
+    totalHighestOrderedProductsPromise,
     averageProductsPricePromise,
-    
   ]);
 
   return {
-    totalProducts, 
+    totalProducts,
     totalProductsByCategory,
     totalProductsByBrandName,
-    averageProductsPrice
-    
-  }
-}
+    totalHighestOrderedProducts,
+    averageProductsPrice,
+  };
+};
 
-const getOrdersStats = async() => {
-  return {}
-}
+const getOrdersStats = async () => {
+  return {};
+};
 
-const getPaymentsStats = async() => {
-  return{}
-}
+const getPaymentsStats = async () => {
+  return {};
+};
 
 export const StatsServices = {
   getUsersStats,
   getProductsStats,
   getOrdersStats,
-  getPaymentsStats
-  
-}
+  getPaymentsStats,
+};
