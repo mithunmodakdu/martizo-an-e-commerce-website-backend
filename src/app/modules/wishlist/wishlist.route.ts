4d3ab2e0b34682f1966a1f2398d90347d4caeb1d
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2026-04-21 16:33:38 +0600

    getWishlist api endpoint created

--- src/app/modules/wishlist/wishlist.route.ts
+++ src/app/modules/wishlist/wishlist.route.ts
@@ -5,7 +5,20 @@ import { ERole } from "../user/user.interface";
 
 const router = Router();
 
-router.patch("/:productId", checkAuth(...Object.values(ERole)), WishlistControllers.removeFromWishlist);
-router.post("/", checkAuth(...Object.values(ERole)), WishlistControllers.addToWishlist);
+router.patch(
+  "/:productId",
+  checkAuth(...Object.values(ERole)),
+  WishlistControllers.removeFromWishlist,
+);
+router.get(
+  "/",
+  checkAuth(...Object.values(ERole)),
+  WishlistControllers.getWishlist,
+);
+router.post(
+  "/",
+  checkAuth(...Object.values(ERole)),
+  WishlistControllers.addToWishlist,
+);
 
 export const WishlistRoutes = router;
