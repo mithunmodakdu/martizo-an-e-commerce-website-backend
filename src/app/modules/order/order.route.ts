577a5d8e3e282bddae3502f0aad9e1561439fcd1
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2026-06-08 23:37:19 +0600

    UpdateOrderZodSchema created

--- src/app/modules/order/order.route.ts
+++ src/app/modules/order/order.route.ts
@@ -3,13 +3,31 @@ import { OrderControllers } from "./order.controller";
 import { checkAuth } from "../../middlewares/checkAuth";
 import { ERole } from "../user/user.interface";
 import { validateRequest } from "../../middlewares/validateRequest";
-import { CreateOrderZodSchema } from "./order.validation";
+import { CreateOrderZodSchema, UpdateOrderZodSchema } from "./order.validation";
 
 const router = Router();
 
-router.get("/:transactionId", checkAuth(...Object.values(ERole)), OrderControllers.getOrderByTransactionId);
-router.patch("/update/:id", checkAuth(ERole.SUPER_ADMIN, ERole.ADMIN), OrderControllers.updateOrderById);
-router.post("/create", checkAuth(...Object.values(ERole)), validateRequest(CreateOrderZodSchema), OrderControllers.createOrder);
-router.get("/", checkAuth(ERole.SUPER_ADMIN, ERole.ADMIN), OrderControllers.getOrders);
+router.get(
+  "/:transactionId",
+  checkAuth(...Object.values(ERole)),
+  OrderControllers.getOrderByTransactionId,
+);
+router.patch(
+  "/update/:id",
+  checkAuth(ERole.SUPER_ADMIN, ERole.ADMIN),
+  validateRequest(UpdateOrderZodSchema),
+  OrderControllers.updateOrderById,
+);
+router.post(
+  "/create",
+  checkAuth(...Object.values(ERole)),
+  validateRequest(CreateOrderZodSchema),
+  OrderControllers.createOrder,
+);
+router.get(
+  "/",
+  checkAuth(ERole.SUPER_ADMIN, ERole.ADMIN),
+  OrderControllers.getOrders,
+);
 
 export const OrderRoutes = router;
