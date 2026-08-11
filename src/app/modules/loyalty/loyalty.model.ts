983955ca4896230bfeb7c1e29b13ccd32b59cc29
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2026-08-11 15:44:31 +0600

    loyaltyTransactionSchema, LoyaltyTransaction model created

--- src/app/modules/loyalty/loyalty.model.ts
+++ src/app/modules/loyalty/loyalty.model.ts
@@ -1,5 +1,5 @@
 import { Schema, model } from "mongoose";
-import { ILoyaltyAccount } from "./loyalty.interface";
+import { ILoyaltyAccount, ILoyaltyTransaction } from "./loyalty.interface";
 
 const loyaltyAccountSchema = new Schema<ILoyaltyAccount>(
   {
@@ -50,3 +50,54 @@ export const LoyaltyAccount = model<ILoyaltyAccount>(
   loyaltyAccountSchema,
 );
 
+const loyaltyTransactionSchema = new Schema<ILoyaltyTransaction>(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    loyaltyAccountId: {
+      type: Schema.Types.ObjectId,
+      ref: "LoyaltyAccount",
+      required: true,
+    },
+    type: {
+      type: String,
+      enum: ["EARN", "REDEEM", "EXPIRE", "REVERSE", "BONUS", "ADJUSTMENT"],
+      required: true,
+    },
+    reason: {
+      type: String,
+      enum: [
+        "ORDER",
+        "SIGNUP",
+        "FIRST_ORDER",
+        "REFERRAL",
+        "REVIEW",
+        "BIRTHDAY",
+        "CAMPAIGN",
+        "ADMIN_ADJUSTMENT",
+      ],
+      required: true,
+    },
+    referenceId: {
+      type: Schema.Types.ObjectId,
+    },
+    referenceType: {
+      type: String,
+      enum: ["ORDER", "REVIEW", "REFERRAL"],
+    },
+    description: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const LoyaltyTransaction = model<ILoyaltyTransaction>(
+  "LoyaltyTransaction",
+  loyaltyTransactionSchema,
+);
