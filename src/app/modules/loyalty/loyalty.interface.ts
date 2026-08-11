455beec63b35c7c9b8d59332ec7edb772f763eed
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2026-08-11 15:10:59 +0600

    TLoyaltyTransactionType, TLoyaltyTransactionReason, ILoyaltyTransaction created

--- src/app/modules/loyalty/loyalty.interface.ts
+++ src/app/modules/loyalty/loyalty.interface.ts
@@ -1,10 +1,6 @@
 import { Types } from "mongoose";
 
-export type TLoyaltyTier =
-  | "BRONZE"
-  | "SILVER"
-  | "GOLD"
-  | "PLATINUM";
+export type TLoyaltyTier = "BRONZE" | "SILVER" | "GOLD" | "PLATINUM";
 
 export interface ILoyaltyAccount {
   userId: Types.ObjectId;
@@ -16,3 +12,41 @@ export interface ILoyaltyAccount {
   createdAt?: Date;
   updatedAt?: Date;
 }
+
+export type TLoyaltyTransactionType =
+  | "EARN"
+  | "REDEEM"
+  | "EXPIRE"
+  | "REVERSE"
+  | "BONUS"
+  | "ADJUSTMENT";
+
+export type TLoyaltyTransactionReason =
+  | "ORDER"
+  | "SIGNUP"
+  | "FIRST_ORDER"
+  | "REFERRAL"
+  | "REVIEW"
+  | "BIRTHDAY"
+  | "CAMPAIGN"
+  | "ADMIN_ADJUSTMENT";
+
+
+export interface ILoyaltyTransaction {
+  userId: Types.ObjectId;
+  loyaltyAccountId: Types.ObjectId;
+
+  type: TLoyaltyTransactionType;
+  reason: TLoyaltyTransactionReason;
+
+  points: number;
+
+  referenceId?: Types.ObjectId;
+  referenceType?: "ORDER" | "REVIEW" | "REFERRAL";
+
+  description?: string;
+
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
