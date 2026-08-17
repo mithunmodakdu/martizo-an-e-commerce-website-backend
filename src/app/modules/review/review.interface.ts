fe791335c78eec00b2f439b15d01a1251f999a38
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2026-08-17 21:45:32 +0600

    EReviewStatus, EVoteType, IReviewImage, IAdminReply, IReview created

---
+++ src/app/modules/review/review.interface.ts
@@ -0,0 +1,44 @@
+import { Types } from "mongoose";
+
+export enum EReviewStatus {
+  PENDING = 'PENDING',
+  APPROVED = 'APPROVED',
+  REJECTED = 'REJECTED',
+}
+
+export enum EVoteType {
+  HELPFUL = 'HELPFUL',
+  UNHELPFUL = 'UNHELPFUL'
+}
+
+export interface IReviewImage {
+  url: string;
+  publicId?: string;
+}
+
+export interface IAdminReply {
+  comment: string;
+  repliedBy: Types.ObjectId;
+  repliedAt: Types.ObjectId;
+}
+
+export interface IReview {
+  productId: Types.ObjectId;
+  userId: Types.ObjectId;
+  orderId?: Types.ObjectId;
+
+  rating: number;
+  title?: string;
+  comment?: string;
+
+  isVerifiedPurchase: boolean;
+
+  images?: IReviewImage[];
+  status: EReviewStatus;
+  helpfulVote: Types.ObjectId[];
+  unhelpfulVote: Types.ObjectId[];
+  adminReply: IAdminReply;
+
+  createdAt?: Date;
+  updatedAt?: Date;
+}
