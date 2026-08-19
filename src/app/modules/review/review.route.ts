86dd14f212552519e9218082148b6e48c05f2dd3
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2026-08-19 15:16:50 +0600

    createReview api endpoint and checkVerifiedPurchase function created

--- src/app/modules/review/review.route.ts
+++ src/app/modules/review/review.route.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { ReviewController } from "./review.controller";
+import { checkAuth } from "../../middlewares/checkAuth";
+import { ERole } from "../user/user.interface";
 
 const router = Router();
 
-router.post("/", ReviewController.createReview)
+router.post("/", checkAuth(...Object.values(ERole)), ReviewController.createReview)
 
 export const ReviewRoutes = router;
--- src/app/modules/review/review.service.ts
+++ src/app/modules/review/review.service.ts
@@ -1,11 +1,59 @@
 import { Types } from "mongoose";
-import { TCreateReviewPayload } from "./review.interface";
+import { EReviewStatus, TCreateReviewPayload } from "./review.interface";
+import { Review } from "./review.model";
+import AppError from "../../errorHelpers/AppError";
+import httpStatusCodes from "http-status-codes";
+import { Order } from "../order/order.model";
 
-const createReview = async(userId: Types.ObjectId, payload: TCreateReviewPayload) => {
-  console.log(userId)
-  console.log(payload)
-}
+const checkVerifiedPurchase = async (
+  userId: Types.ObjectId,
+  productId: Types.ObjectId,
+): Promise<{isVerified: boolean; orderId?: Types.ObjectId}> => {
+  const order = await Order.findOne({
+    userId,
+    status: "DELIVERED",
+    "items.productId": productId,
+  }).select("_id");
+
+  return order
+    ? { isVerified: true, orderId: order._id }
+    : { isVerified: false };
+};
+
+const createReview = async (
+  userId: Types.ObjectId,
+  payload: TCreateReviewPayload,
+) => {
+  const existedReview = await Review.findOne({
+    userId,
+    productId: payload.productId,
+  });
+
+  if (existedReview) {
+    throw new AppError(
+      httpStatusCodes.CONFLICT,
+      "You have already reviewed this product",
+    );
+  }
+
+  const verifiedPurchase = await checkVerifiedPurchase(
+    userId,
+    payload.productId,
+  );
+
+  const review = await Review.create({
+    ...payload,
+    userId,
+    orderId: verifiedPurchase.orderId,
+    isVerifiedPurchase: verifiedPurchase.isVerified,
+    status: EReviewStatus.PENDING,
+    helpfulVote: [],
+    unhelpfulVote: [],
+  });
+  
+  return review;
+};
 
 export const ReviewService = {
-  createReview
-}
+  createReview,
+};
